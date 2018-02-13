e18c2aacededf37383bd0634373c58a6c7e8a65f
Leonardo Rocha <leonardolpi@gmail.com>
2018-02-13 10:44:27 -0200

    Improve run site

--- run.cmd
+++ run.cmd
@@ -1,2 +1,7 @@
-call ..\..\..\virtualenv\django_1.11\Scripts\activate.bat
+REM call ..\..\..\virtualenv\django_1.11\Scripts\activate.bat
+REM python manage.py runserver
+
+python -m virtualenv virtualenv
+call virtualenv\Scripts\activate.bat
+pip instal -r requirements.txt
 python manage.py runserver
