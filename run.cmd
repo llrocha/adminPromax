5057575d15a19572753ab87f894e9a35a1aa7362
Leonardo Rocha <leonardolpi@gmail.com>
2018-02-13 13:47:25 -0200

    Create virtualenv if not exists

--- run.cmd
+++ run.cmd
@@ -1,7 +1,8 @@
 REM call ..\..\..\virtualenv\django_1.11\Scripts\activate.bat
 REM python manage.py runserver
 
+python -m pip install virtualenv --upgrade
 python -m virtualenv virtualenv
 call virtualenv\Scripts\activate.bat
-pip instal -r requirements.txt
+python -m pip install -r requirements.txt
 python manage.py runserver
