59e0dcf6ecd758f279f5fc6f1b7a004bc18801e1
root <root@localhost.hbsisnet.local>
2018-02-13 11:35:47 -0200

    Script for run controlServer

---
+++ run_controlserver.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+. /opt/rh/rh-python36/enable
+
+if [ ! -d venv/bin/ ]
+then
+  pip3 install virtualenv --upgrade
+  virtualenv venv
+fi
+
+. venv/bin/activate
+
+pip3 install -r requirements.txt
+
+cd controlServer
+
+#./start.sh
+export PYTHONPATH=/root/adminPromax
+python controlserver.py
